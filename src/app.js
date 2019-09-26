a293559c3557498031e6bd78b825a4b331ec0811
Steven Bull <stevenwbulljr@gmail.com>
2019-09-26 14:40:31 -0500

    add users variable with fake data

--- src/app.js
+++ src/app.js
@@ -7,6 +7,23 @@ const { NODE_ENV } = require('./config');
 
 const app = express();
 
+const users = [
+  {
+    'id': '3c8da4d5-1597-46e7-baa1-e402aed70d80',
+    'username': 'sallyStudent',
+    'password': 'c00d1ng1sc00l',
+    'favoriteClub': 'Cache Valley Stone Society',
+    'newsLetter': 'true'
+  },
+  {
+    'id': 'ce20079c-2326-4f17-8ac4-f617bfd28b7f',
+    'username': 'johnBlocton',
+    'password': 'veryg00dpassw0rd',
+    'favoriteClub': 'Salt City Curling Club',
+    'newsLetter': 'false'
+  }
+];
+
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
