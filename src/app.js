bf401fbfdcb57c066c69d3981346d968279bea7a
Steven Bull <stevenwbulljr@gmail.com>
2019-09-26 14:28:16 -0500

    add validation in app.js

--- src/app.js
+++ src/app.js
@@ -27,6 +27,31 @@ app.post('/', ( req, res ) => {
 
 app.post('/user', ( req, res ) => {
   const { username, password, favoriteClub, newsLetter = false } = req.body;
+
+  if (!username) {
+    return res.status(400).send('Username required');
+  }
+
+  if (!password) {
+    return res.status(400).send('Password required');
+  }
+
+  if (!favoriteClub) {
+    return res.status(400).send('favorite club required');
+  }
+
+  if (username.length < 6 || username.length > 20) {
+    return res.status(400).send('Username must be between 6 and 20 characters');
+  }
+
+  if (password.length < 8 || password.length > 36) {
+    return res.status(400).send('Password must be between 8 and 36 characters');
+  }
+
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res.status(400).send('Password must contain at least one digit');
+  }
+  
 });
 
 app.use(function errorHandler( error, req, res, next ) {
